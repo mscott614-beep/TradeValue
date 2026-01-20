398090ab56bab2fc3da0ac0ab9c4d822499d7ecc
Mike Scott <mscott614@gmail.com>
2026-01-20 02:01:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "puckvaluebak-38609945-5e85c",
+  "appId": "1:103832809240:web:a4d5addf78f0638d83386d",
+  "apiKey": "AIzaSyDID3Obctz23xrI56grM_kvDeuoElkfg7A",
+  "authDomain": "puckvaluebak-38609945-5e85c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "103832809240"
+};
